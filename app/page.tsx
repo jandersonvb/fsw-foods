4664bf5a116e0392f7bc6d2350179c9ad68d06b3
jandersonvb <jandersonvb.dev@gmail.com>
2024-05-01 19:29:58 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 const Home = () => {
-  return <Header />;
+  return (
+    <div>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
